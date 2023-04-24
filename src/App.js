6abfae1c908df7f7ff52362c3f0f2080f17a6e66
= <bibekkarki662@gmail.com>
2023-04-24 14:36:37 +0545

    stateful component

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import Avatar from "./components/Avatar";
 import Greet from "./components/Greet";
 import { Profiledata } from "./module/profile";
+import Welcome  from "./components/Welcome";
 
 function App() {
   return (
     <div className="App">
       <Avatar user={Profiledata.user} />
       <Greet name={Profiledata.user.name} />
+      <Welcome />
     </div>
   );
 }
