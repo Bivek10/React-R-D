11e11638945c8a06cac7581e4c86678d488663cc
= <bibekkarki662@gmail.com>
2023-04-24 11:08:32 +0545

    arrow function

--- src/components/Greet.js
+++ src/components/Greet.js
@@ -1,8 +1,8 @@
 import React from "react";
 
-function Greet(){
-    return <h1>Hello Bivek </h1>
-}
-
+// function Greet(){
+//     return <h1>Hello Bivek </h1>
+// }
+const Greet = ()=> <h1>Hello Bivke</h1>
 
 export default Greet
