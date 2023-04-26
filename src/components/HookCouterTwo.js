6c48d06a8f22957b773745865431053bc8705f09
= <bibekkarki662@gmail.com>
2023-04-26 12:18:31 +0545

    access previous state and update new state

--- src/components/HookCouterTwo.js
+++ src/components/HookCouterTwo.js
@@ -5,12 +5,21 @@ function HookCounterTwo() {
 
   const [count, setCount] = useState(initialCount);
 
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+
   return (
     <div>
       <h1>Count: {count}</h1>
-      <button onClick={() => setCount(count + 1)}> Increment</button>
-      <button onClick={() => setCount(initialCount)}> Reset</button>
-      <button onClick={() => setCount(count - 1)}> Decrement </button>
+      <button onClick={() => setCount(prevCount => prevCount+1)}> Increment</button>
+      <button onClick={() => setCount(prevCount => prevCount=0)}> Reset</button>
+
+      <button onClick={() => setCount(prevCount=> prevCount-1)}> Decrement </button>
+
+      <button onClick={incrementFive}> Increment by 5 </button>
     </div>
   );
 }
