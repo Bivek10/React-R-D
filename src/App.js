5bc123e7267b9b20533a282980b4fb4031bab47e
= <bibekkarki662@gmail.com>
2023-05-01 16:53:23 +0545

    Routing and Navigation

--- src/App.js
+++ src/App.js
@@ -1,55 +1,26 @@
-import "./App.css";
-import ComponentD from "./components/UseContextEx";
-import UseReducerEx from "./components/UseReducerEx";
-import ComponentE from "./components/componentE";
-import ParentCompoent from "./components/useCallbacksEx/ParentCompoent"
-import CounterMemo from "./components/useMemoHookEx/CounterMemo";
-import FocusInput from './components/useRefHook/FocusInput'
-import HookTimer from "./components/useRefHook/HookTimer";
 import React from "react";
-
-
+import "./App.css";
+import Home from "./components/routing/Home";
+import About from "./components/routing/About";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Profile from "./components/routing/Profile";
+import ContactUs from "./components/routing/ContactUs";
+import Error from "./components/routing/Error";
 export const UserContext = React.createContext();
 export const AddressContext = React.createContext();
 
 function App() {
   return (
     <div className="App">
-      {/* <userContext.Provider value={{username:"Bivek Karki"}}>
-      <UseContextExOne/>
-      </userContext.Provider> */}
-      <userContext.Provider value={{username:"Bivek Karki"}}>
-        <UseContextExTwo />
-      </userContext.Provider>
-
-      <HookTimer/>
-      {/* <UserContext.Provider value={"Bivek Karki"}>
-        <AddressContext.Provider value={"Dilibazar, Kathamndu"}>
-         <ComponentD/>
-          <ComponentE/>
-        </AddressContext.Provider>
-      </UserContext.Provider> */}
-      {/* <CounterMemo/> */}
-      {/* <FocusInput/> */}
-     {/* <ParentCompoent/> */}
-      {/* <UseReducerEx/> */}
-      {/* <HookDataFetch /> */}
-      {/* <IntervalHookCounter/> */}
-      {/* <MouseContainer/> */}
-      {/* <HookMouse/> */}
-      {/* <HookCounterFive/> */}
-      {/* <ClassCounterOne/> */}
-      {/* <HookCounterFour/> */}
-      {/* <HookCounterThree /> */}
-      {/* <HookCounterTwo/> */}
-      {/* <Timer/> */}
-      {/* <Counter /> */}
-      {/* <SubscribeComp/> */}
-      {/* <Avatar user={Profiledata.user} />
-      <Greet name={Profiledata.user.name} />
-      <Welcome /> */}
-      {/* <BookList bookData= {BookData} /> */}
-      {/* <RenderPersonList data={people} /> */}
+      <BrowserRouter>
+        <Routes>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/profile" element={<Profile />}></Route>
+          <Route path="/contactus" element={<ContactUs />}></Route>
+          <Route path="*" element={<Error/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
